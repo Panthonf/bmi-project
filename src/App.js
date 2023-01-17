ec984dc9f39e55ac6dd26004e11c26146819b403
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-01-17 13:42:00 +0700

    commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BMI from "./component/bmi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center font-prompt">
+      <div className="mt-8 max-w-min p-8 rounded-lg shadow-lg">
+        <div className="font text-blue-700 font-sans">
+          <h2 className="font-bold flex justify-center text-xl">คำนวณค่าดัชนีมวลกาย (BMI)</h2>
+          <p class="text" className="mt-4">
+            ส่วนสูง (เมตร)
+          </p>
+          <input
+            type="text"
+            id="h"
+            className="my-2 rounded-md border-2 border-blue-300 p-2"
+          />
+          <p class="text" className="mt-2">
+            นำ้หนัก (กิโลกรัม)
+          </p>
+          <input
+            type="text"
+            id="w"
+            className="my-2 rounded-md border-2 border-blue-300 p-2"
+          />
+          
+          <div className="flex justify-center my-2">
+            <button
+              id="btn"
+              onClick={BMI}
+              className="font-extrabold rounded-md bg-blue-600 p-2 m-2 text-blue-50 hover:bg-green-500"
+            >
+              คำนวณ
+            </button>
+          </div>
+
+          <p id="result" className="flex justify-center"></p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/bmi.js
@@ -0,0 +1,16 @@
+export default function BMI() {
+  var h = document.getElementById("h").value;
+  var w = document.getElementById("w").value;
+
+  var bmi = w / (h * h);
+  var bmio = bmi.toFixed(2);
+
+  if (h.length === 0 || w.length === 0) {
+    document.getElementById("result").style.color = "red";
+    document.getElementById("result").innerHTML =
+      "! กรุณาใส่ส่วนสูงและนำ้หนักให้ครบถ้วน";
+  }else {
+    document.getElementById("result").innerHTML =
+      "ดัชนีมวลกายของคุณ คือ " + bmio;
+  }
+}
