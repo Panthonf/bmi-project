9410b7193a42946314857dd03e293cc047b1c204
Panthon Kansap <panthon_kansap@cmu.ac.th>
2023-01-17 23:10:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
 import "./App.css";
 import BMI from "./component/bmi";
-import React from 'react';
-import logo1 from './img/logo1.png'
-import logo2 from './img/logo2.png'
+import React from "react";
+import logo1 from "./img/logo1.png";
+import logo2 from "./img/logo2.png";
+import bmi1 from "./img/bmi1.jpg";
 
 function App() {
   return (
-    
-    
-      <div>
-        <div className="flex justify-start mt-4 ml-8">
-          <img src={logo1} alt="Girl in a jacket" className="mx-2 h-32 my-auto"></img>
-          <img src={logo2} alt="Girl in a jacket" className="h-40 my-auto"></img>
-        </div>
-        <div className="font-prompt mt-2 text-blue-900">
-          <h1 className="flex justify-center text-xl">สถาบันพระบรมราชชนนีสวรรค์ประชารักษ์ นครสวรรค์</h1>
-          <h1 className="flex justify-center text-lg">สถาบันบรมราชชนก</h1>
-        </div>
-        <div className="flex justify-center font-prompt">
-      <div className="mt-8 max-w-min p-8 rounded-lg shadow-lg">
-        <div className="font text-blue-700 font-sans">
-          <h2 className="font-bold flex justify-center text-xl">คำนวณค่าดัชนีมวลกาย (BMI)</h2>
-          <p class="text" className="mt-4">
-            ส่วนสูง (เมตร)
-          </p>
-          <input
-            type="text"
-            id="h"
-            className="my-2 rounded-md border-2 border-blue-300 p-2"
-          />
-          <p class="text" className="mt-2">
-            นำ้หนัก (กิโลกรัม)
-          </p>
-          <input
-            type="text"
-            id="w"
-            className="my-2 rounded-md border-2 border-blue-300 p-2"
-          />
-          
-          <div className="flex justify-center my-2">
-            <button
-              id="btn"
-              onClick={BMI}
-              className="font-extrabold rounded-md bg-blue-600 p-2 m-2 text-blue-50 hover:bg-green-500"
-            >
-              คำนวณ
-            </button>
-          </div>
+    <div className="mb-4">
+      <div className="flex justify-center mt-4 ml-8">
+        <img src={logo1} alt="logo1" className="mx-2 h-24 my-auto"></img>
+        <img src={logo2} alt="logo2" className="h-28 my-auto"></img>
+      </div>
+      <div className="font-prompt mt-2 text-blue-900">
+        <h1 className="flex justify-center text-xl">
+          สถาบันพระบรมราชชนนีสวรรค์ประชารักษ์ นครสวรรค์
+        </h1>
+        <h1 className="flex justify-center text-lg">สถาบันบรมราชชนก</h1>
+      </div>
+      <div className="flex justify-center font-prompt">
+        <div className="mt-4 max-w-min p-8 rounded-lg shadow">
+          <div className="font text-blue-700 font-sans">
+            <h2 className="font-bold flex justify-center text-xl">
+              คำนวณค่าดัชนีมวลกาย (Body Mass Index)
+            </h2>
+            <p class="text" className="mt-4">
+              ส่วนสูง (เมตร)
+            </p>
+            <input
+              type="text"
+              id="h"
+              className="my-2 rounded-md border-2 border-blue-300 p-2"
+            />
+            <p class="text" className="mt-2">
+              นำ้หนัก (กิโลกรัม)
+            </p>
+            <input
+              type="text"
+              id="w"
+              className="my-2 rounded-md border-2 border-blue-300 p-2"
+            />
 
-          <p id="result" className="flex justify-center"></p>
+            <div className="flex justify-center my-2">
+              <button
+                id="btn"
+                onClick={BMI}
+                className="font-extrabold rounded-md bg-blue-600 p-2 m-2 text-blue-50 hover:bg-green-500"
+              >
+                คำนวณ
+              </button>
+            </div>
+
+            <p id="result" className="flex justify-center"></p>
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center mt-2">
+        <img src={bmi1} alt="bmi-level" className="h-96 rounded-md"></img>
       </div>
     </div>
-      </div>
-    
   );
 }
 
